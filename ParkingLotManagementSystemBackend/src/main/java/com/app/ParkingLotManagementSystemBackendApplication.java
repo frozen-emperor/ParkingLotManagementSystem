c1c30c0169c3c6d17eb9885a35c5be0e5e8f69a8
frozen-emperor <mail.shailesh.singh@gmail.com>
2022-03-12 13:47:58 +0530

    init - dev profile,domain docs

---
+++ ParkingLotManagementSystemBackend/src/main/java/com/app/ParkingLotManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotManagementSystemBackendApplication.class, args);
+	}
+
+}
